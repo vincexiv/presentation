02f1147a7da4ba13c1fc37b002d194e1404476fc
Vincent <makokhavomondi@gmail.com>
2023-06-10 13:37:55 +0300

    Start working on the layout list

---
+++ src/components/CanvasLayoutList/CanvasLayoutList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div v-for="layout in layoutList" :key="layout.id">
+        <div v-for="structure in layout.structure" :key="structure.id">
+            <div :style="getStyle(structure)"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+import layoutList from './layoutList'
+
+export default
+
+</script>
+<style>
+</style>
---
+++ src/components/CanvasLayoutList/layoutlist.js
@@ -0,0 +1,19 @@
+const layoutList = [
+    {
+        id: 1,
+        layoutStructure: [
+            { id: "content-1", type: "text", top: "5%", left: "5%", width: "90%", height: "10%" },
+            { id: "content-2", type: "text", top: "7%", left: "5%", width: "90%", height: "84%%" }
+        ]
+    },
+    {
+        id: 2,
+        layoutStructure: [
+            { id: "content-3", type: "text", top: "5%", left: "5%", width: "90%", height: "10%" },
+            { id: "content-4", type: "chart", top: "7%", left: "5%", width: "44%", height: "84%%" },
+            { id: "content-5", type: "text", top: "7%", left: "51%", width: "44%", height: "84%%" }
+        ]
+    }
+]
+
+export default layoutList;
