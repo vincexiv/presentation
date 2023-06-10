23ff7ecd2d72cb2817f0610d4242ebe1e0f43dd3
Vincent <makokhavomondi@gmail.com>
2023-06-10 15:01:08 +0300

    Add default content

--- src/utilities/data/layoutlist.js
+++ src/utilities/data/layoutlist.js
@@ -2,16 +2,16 @@ const layoutList = [
     {
         id: 1,
         structure: [
-            { id: "content-1", type: "text", style: {top: "5%", left: "5%", width: "90%", height: "20%" }},
-            { id: "content-2", type: "text", style: {top: "30%", left: "5%", width: "90%", height: "65%" }}
+            { id: "content-1", type: "text", defaultContent: "Click to add title", style: {top: "5%", left: "5%", width: "90%", height: "20%" }},
+            { id: "content-2", type: "text", defaultContent: "Click to add text", style: {top: "30%", left: "5%", width: "90%", height: "65%" }}
         ]
     },
     {
         id: 2,
         structure: [
-            { id: "content-3", type: "text", style: {top: "5%", left: "5%", width: "90%", height: "20%" }},
-            { id: "content-4", type: "chart", style: {top: "30%", left: "5%", width: "44%", height: "65%" }},
-            { id: "content-5", type: "text", style: {top: "30%", left: "51%", width: "44%", height: "65%" }}
+            { id: "content-3", type: "text", defaultContent: "Click to add title", style: {top: "5%", left: "5%", width: "90%", height: "20%" }},
+            { id: "content-4", type: "chart", defaultContent: "Click to add text", style: {top: "30%", left: "5%", width: "44%", height: "65%" }},
+            { id: "content-5", type: "text", defaultContent: "Click to add text", style: {top: "30%", left: "51%", width: "44%", height: "65%" }}
         ]
     }
 ]
