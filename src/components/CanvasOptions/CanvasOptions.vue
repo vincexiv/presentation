3d4481aed568836c2e1a79e56759d8a57aa1e929
Vincent <makokhavomondi@gmail.com>
2023-06-15 19:54:02 +0300

    Update styling of active item in CanvasOptions component

--- src/components/CanvasOptions/CanvasOptions.vue
+++ src/components/CanvasOptions/CanvasOptions.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="canvas-options">
-        <button v-for="option in options" :key="option">
+    <div class="canvas-options" ref="target">
+        <button v-for="option in options" :key="option" :id="option" @click="makeActive">
             {{ option }}
         </button>
     </div>
@@ -12,8 +12,30 @@ export default ({
     props: {
         updateActiveOption: Function,
         options: Array
+    },
+    mounted: function(){
+        const activeOption = this.$refs.target.querySelector(`#${this.activeOption}`)
+        activeOption.classList.add("active")
+    },
+    methods: {
+        makeActive: function(e){
+            const activeButton = e.target
+            const allOptions = this.$refs.target.querySelectorAll('button')
+
+            allOptions.forEach(option => {
+                option.classList.remove('active')
+            })
+
+            activeButton.classList.add('active')
+        }
+    },
+    data(){
+        return {
+            activeOption: this.options[0]
+        }
     }
 })
+
 </script>
 
 <style scoped>
@@ -33,5 +55,9 @@ export default ({
     display: flex;
     align-items: center;
     justify-content: center;
+}
+
+.canvas-options button.active {
+    background-color: #949393;
 }
 </style>
