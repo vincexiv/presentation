5a544544ae26350ad8c4e388d25085477941f42b
Vincent <makokhavomondi@gmail.com>
2023-06-15 17:15:25 +0300

    Update layout list

--- src/utilities/data/layoutlist.js
+++ src/utilities/data/layoutlist.js
@@ -2,16 +2,45 @@ const layoutList = [
     {
         id: 1,
         structure: [
-            { id: "content-1", type: "text", defaultContent: "Click to add title", style: {top: "5%", left: "5%", width: "90%", height: "20%" }},
-            { id: "content-2", type: "text", defaultContent: "Click to add text", style: {top: "30%", left: "5%", width: "90%", height: "65%" }}
+            { 
+                id: "content-1", 
+                type: "text", 
+                typeDetails: { category: "title" },
+                defaultContent: "Click to add title", 
+                style: {top: "5%", left: "5%", width: "90%", height: "20%" }
+            },
+            { 
+                id: "content-2", 
+                type: "text", 
+                typeDetails: { category: "content" },
+                defaultContent: "Click to add text", 
+                style: {top: "30%", left: "5%", width: "90%", height: "65%" }
+            }
         ]
     },
     {
         id: 2,
         structure: [
-            { id: "content-3", type: "text", defaultContent: "Click to add title", style: {top: "5%", left: "5%", width: "90%", height: "20%" }},
-            { id: "content-4", type: "chart", defaultContent: "Click to add text", style: {top: "30%", left: "5%", width: "44%", height: "65%" }},
-            { id: "content-5", type: "text", defaultContent: "Click to add text", style: {top: "30%", left: "51%", width: "44%", height: "65%" }}
+            { 
+                id: "content-3", 
+                type: "text", 
+                typeDetails: { category: "title" },
+                defaultContent: "Click to add title", 
+                style: {top: "5%", left: "5%", width: "90%", height: "20%" }
+            },
+            { 
+                id: "content-4", 
+                type: "chart", 
+                defaultContent: "Click to add text", 
+                style: {top: "30%", left: "5%", width: "44%", height: "65%" }
+            },
+            { 
+                id: "content-5", 
+                type: "text", 
+                typeDetails: { category: "content" },
+                defaultContent: "Click to add text", 
+                style: {top: "30%", left: "51%", width: "44%", height: "65%" }
+            }
         ]
     }
 ]
