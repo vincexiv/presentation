fecb7f61d1db30a0fd83fb070a7600144b5e44f5
Vincent <makokhavomondi@gmail.com>
2023-06-15 20:37:59 +0300

    Add more layout options

--- src/utilities/data/layoutlist.js
+++ src/utilities/data/layoutlist.js
@@ -30,6 +30,32 @@ const layoutList = [
             },
             { 
                 id: "content-4", 
+                type: "text", 
+                typeDetails: {category: "content"},
+                defaultContent: "Click to add text", 
+                style: {top: "30%", left: "5%", width: "44%", height: "65%" }
+            },
+            { 
+                id: "content-5", 
+                type: "text", 
+                typeDetails: {category: "content"},
+                defaultContent: "Click to add text", 
+                style: {top: "30%", left: "51%", width: "44%", height: "65%" }
+            }
+        ]
+    },
+    {
+        id: 3,
+        structure: [
+            { 
+                id: "content-3", 
+                type: "text", 
+                typeDetails: { category: "title" },
+                defaultContent: "Click to add title", 
+                style: {top: "5%", left: "5%", width: "90%", height: "20%" }
+            },
+            { 
+                id: "content-4", 
                 type: "chart", 
                 defaultContent: "Click to add text", 
                 style: {top: "30%", left: "5%", width: "44%", height: "65%" }
@@ -38,6 +64,31 @@ const layoutList = [
                 id: "content-5", 
                 type: "text", 
                 typeDetails: { category: "content" },
+                defaultContent: "Click to add text", 
+                style: {top: "30%", left: "51%", width: "44%", height: "65%" }
+            }
+        ]
+    },
+    {
+        id: 3,
+        structure: [
+            { 
+                id: "content-3", 
+                type: "text", 
+                typeDetails: { category: "title" },
+                defaultContent: "Click to add title", 
+                style: {top: "5%", left: "5%", width: "90%", height: "20%" }
+            },
+            { 
+                id: "content-4", 
+                type: "text", 
+                typeDetails: { category: "content" },
+                defaultContent: "Click to add text", 
+                style: {top: "30%", left: "5%", width: "44%", height: "65%" }
+            },
+            { 
+                id: "content-5", 
+                type: "chart", 
                 defaultContent: "Click to add text", 
                 style: {top: "30%", left: "51%", width: "44%", height: "65%" }
             }
