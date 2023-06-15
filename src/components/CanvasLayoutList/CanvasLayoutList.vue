e2e0774932b6a3e7b407002f29c3d7f526b05dcf
Vincent <makokhavomondi@gmail.com>
2023-06-15 17:37:50 +0300

    Sticky layoutlist

--- src/components/CanvasLayoutList/CanvasLayoutList.vue
+++ src/components/CanvasLayoutList/CanvasLayoutList.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="layout-list-container">
-        <div v-for="layout in layoutList" :key="layout.id" class="layout-list-object" @click="updateLayout(layout)">
-            <div v-for="layoutStructure in layout.structure" :key="layoutStructure.id">
-                <div v-if="layoutStructure.type == 'chart'"
-                    :class="layoutStructure.type"
-                    :style="layoutStructure.style"
-                    class="layout-object">
-                </div>
-                <div v-else-if="layoutStructure.type == 'text'"
-                    :class="layoutStructure.type"
-                    :style="layoutStructure.style"
-                    class="layout-object">
+        <div class="sticky">
+            <div v-for="layout in layoutList" :key="layout.id" class="layout-list-object" @click="updateLayout(layout)">
+                <div v-for="layoutStructure in layout.structure" :key="layoutStructure.id">
+                    <div v-if="layoutStructure.type == 'chart'"
+                        :class="layoutStructure.type"
+                        :style="layoutStructure.style"
+                        class="layout-object">
+                    </div>
+                    <div v-else-if="layoutStructure.type == 'text'"
+                        :class="layoutStructure.type"
+                        :style="layoutStructure.style"
+                        class="layout-object">
+                    </div>
                 </div>
             </div>
         </div>
@@ -50,16 +52,19 @@ export default {
 </script>
 
 <style>
-.layout-list-container {
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-}
 .layout-list-object {
     position: relative;
     width: 10rem;
     height: 7rem;
     outline: solid #d4d4d4 0.15rem;
+}
+
+.layout-list-container .sticky {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    position: sticky;
+    top: 5rem;
 }
 
 .layout-list-object .layout-object {
