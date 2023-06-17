c8e889f32c0ec53cfc158696f172920698e3b973
Vincent <makokhavomondi@gmail.com>
2023-06-17 15:12:24 +0300

    Only close modal when outer side of the modal has been clicked
     Also start working on adding chart items to the modal that can be added to the slides

--- src/components/ChartModal/ChartModal.vue
+++ src/components/ChartModal/ChartModal.vue
@@ -1,16 +1,44 @@
 <template>
-    <div class="chart-modal-container" @click="closeModal">
-        <div class="chart-modal">
-
+    <div class="chart-modal-container" @click="handleModalClick">
+        <div class="chart-modal" @click="handleModalClick">
+            <div class="chart-container">
+                <vue-2-highcharts 
+                    v-for="object in highchartObjectArray"
+                    :options="object"
+                    :key="object.id"
+                    class="modal-highchart">
+                </vue-2-highcharts>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import highchartObjectArray from '@/utilities/data/highchartObjectArray';
+import Vue2Highcharts from 'vue2-highcharts'
+
 export default({
     name: "ChartModal",
     props: {
         closeModal: Function
+    },
+    components: {
+        Vue2Highcharts
+    },
+    data(){
+        return {
+            highchartObjectArray: highchartObjectArray
+        }
+    },
+    methods: {
+        handleModalClick: function(e){
+            const classList = Array.from(e.target.classList)
+
+            // Close modal only if the outer side has been clicked
+            if(classList.find(domItemClass => domItemClass === 'chart-modal-container')){
+                this.closeModal()
+            }
+        }
     }
 })
 </script>
