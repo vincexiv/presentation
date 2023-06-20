c17d0fa3e307be95f1ba2c3eb6b091602e9053f9
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-20 19:22:39 +0300

    object editor

--- src/components/CanvasObjectEditor/CanvasObjectEditor.vue
+++ src/components/CanvasObjectEditor/CanvasObjectEditor.vue
@@ -1,47 +1,103 @@
 <template>
-    <div class="canvas-object-editor-container">
-
-        <div>
-            <textarea v-model="text" :style="textStyle"></textarea>
-        </div>
+  <div>
+    <div class="canvas">
+      <div v-for="object in canvasObjects" :key="object.id" :style="object.style">
+        <!-- Render the object based on its type -->
+        <template v-if="object.type === 'text'">
+          <div class="text-object">
+            <div
+              class="text-content"
+              contenteditable
+              @input="updateTextContent(object.id, $event.target.textContent)"
+              @focus="highlightObject(object.id)"
+              @blur="removeHighlight()"
+            >
+              {{ object.content.text }}
+            </div>
+          </div>
+        </template>
+        <template v-else-if="object.type === 'chart'">
+          <div class="chart-object">
+            <!-- Render the chart using the provided chart library -->
+            <!-- ... -->
+          </div>
+        </template>
+      </div>
     </div>
-    
+    <div class="editing-options">
+      <button @click="applyBold">Bold</button>
+      <button @click="applyItalic">Italic</button>
+      <button @click="applyColor('red')">Red</button>
+      <!-- Add more editing options as needed -->
+    </div>
+  </div>
 </template>
 
 <script>
-    export default ({
-        name: "CanvasObjectEditor",
-        data() {
+export default {
+  data() {
     return {
-      text: '',
-      isBold: false,
-      isItalic: false,
-      isUnderline: false
+      canvasObjects: [] // Your canvas objects data from SampleData.js
     };
   },
-  computed: {
-    textStyle() {
-      let style = '';
-      if (this.isBold) style += 'font-weight: bold;';
-      if (this.isItalic) style += 'font-style: italic;';
-      if (this.isUnderline) style += 'text-decoration: underline;';
-      return style;
-    }
-  },
   methods: {
-    toggleBold() {
-      this.isBold = !this.isBold;
+    updateTextContent(objectId, newText) {
+      // Find the corresponding object in canvasObjects and update its content
+      const object = this.canvasObjects.find(obj => obj.id === objectId);
+      if (object) {
+        object.content.text = newText;
+      }
     },
-    toggleItalic() {
-      this.isItalic = !this.isItalic;
+    highlightObject(objectId) {
+      // Update the style of the highlighted object
+      const object = this.canvasObjects.find(obj => obj.id === objectId);
+      if (object) {
+        object.style.backgroundColor = 'yellow';
+      }
     },
-    toggleUnderline() {
-      this.isUnderline = !this.isUnderline;
-    }
+    removeHighlight() {
+      // Remove the highlight style from all objects
+      this.canvasObjects.forEach(obj => {
+        obj.style.backgroundColor = 'transparent';
+      });
+    },
+    applyBold() {
+      // Apply bold style to the selected object
+      // Implement your logic here
+    },
+    applyItalic() {
+      // Apply italic style to the selected object
+      // Implement your logic here
+    },
+    applyColor() {
+      // Apply color style to the selected object
+      // Implement your logic here
+    },
+    // Add more methods for other editing options as needed
   }
-    })
-
+};
 </script>
+
+<style>
+.canvas {
+  position: relative;
+}
+
+.text-object {
+  position: absolute;
+}
+
+.chart-object {
+  /* Styles for chart objects */
+  position: relative;
+}
+
+.editing-options {
+  /* Styles for editing options */
+  position: inherit;
+}
+</style>
+
 
 <style scoped>
 .canvas-object-editor-container {
