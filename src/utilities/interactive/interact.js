7994184d685a2ef9bd628a0be6bd3a642bdf1c6c
Vincent <makokhavomondi@gmail.com>
2023-06-27 17:01:31 +0300

    Create a "makeDraggable" function that makes an object only draggable

--- src/utilities/interactive/interact.js
+++ src/utilities/interactive/interact.js
@@ -80,5 +80,34 @@ function makeResizableAndDraggable(cssSelector, chart){
       })
 }
 
+function makeDraggable(cssSelector){
+  interact(cssSelector).draggable({
+    listeners: { move: function dragMoveListener (event) {
+      var target = event.target,
+          // keep the dragged position in the data-x/data-y attributes
+          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+  
+      // translate the element
+      target.style.webkitTransform =
+      target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
+  
+      // update the posiion attributes
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+    } 
+  },
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ]
+  })
+}
+
 
 export  default makeResizableAndDraggable;
+export {makeDraggable}
