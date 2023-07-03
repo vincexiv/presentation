be5337b64157490ce6b571ab4120bc232eebd36c
Vincent <makokhavomondi@gmail.com>
2023-07-03 15:32:11 +0300

    Minor formatting to make things look a bit cleaner

--- src/utilities/interactive/interact.js
+++ src/utilities/interactive/interact.js
@@ -16,84 +16,84 @@ function makeResizableAndDraggable(cssSelector, chart, canvasId){
   const PixelsToVW = canvasWidthVW/canvasWidthPixels
   console.log(PixelsToVW)
 
+
+  interact(cssSelector)
+    .resizable({
+      // resize from all edges and corners
+      edges: { left: true, right: true, bottom: true, top: true },
   
-    interact(cssSelector)
-      .resizable({
-        // resize from all edges and corners
-        edges: { left: true, right: true, bottom: true, top: true },
-    
-        listeners: {
-          move (event) {
-            var target = event.target
-            var x = (parseFloat(target.getAttribute('data-x')) || 0)
-            var y = (parseFloat(target.getAttribute('data-y')) || 0)
-    
-            // update the element's style
-            target.style.width = event.rect.width + 'px'
-            target.style.height = event.rect.height + 'px'
-    
-            // translate when resizing from top or left edges
-            x += event.deltaRect.left
-            y += event.deltaRect.top
-    
-            target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
-    
-            target.setAttribute('data-x', x)
-            target.setAttribute('data-y', y)
-            // target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+      listeners: {
+        move (event) {
+          var target = event.target
+          var x = (parseFloat(target.getAttribute('data-x')) || 0)
+          var y = (parseFloat(target.getAttribute('data-y')) || 0)
+  
+          // update the element's style
+          target.style.width = event.rect.width + 'px'
+          target.style.height = event.rect.height + 'px'
+  
+          // translate when resizing from top or left edges
+          x += event.deltaRect.left
+          y += event.deltaRect.top
+  
+          target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+  
+          target.setAttribute('data-x', x)
+          target.setAttribute('data-y', y)
+          // target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
 
-            // Resize the chart when the
-            // the container is resized
-            if(chart){
-              chart.reflow()
-            }
+          // Resize the chart when the
+          // the container is resized
+          if(chart){
+            chart.reflow()
           }
-        },
-        modifiers: [
-          // keep the edges inside the parent
-          interact.modifiers.restrictEdges({
-            outer: 'parent'
-          }),
-    
-          // minimum size
-          interact.modifiers.restrictSize({
-            min: { width: 20, height: 20 }
-          })
-        ],
-    
-        inertia: true
-      })
-      .draggable({
-        listeners: { move: function dragMoveListener (event) {
-          var target = event.target,
-              // keep the dragged position in the data-x/data-y attributes
-              x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-              y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-      
-          // translate the element
-          target.style.webkitTransform =
-          target.style.transform =
-            'translate(' + x + 'px, ' + y + 'px)';
-      
-          // update the posiion attributes
-          target.setAttribute('data-x', x);
-          target.setAttribute('data-y', y);
-
-            // Resize the chart when the
-            // the container is resized
-            if(chart){
-              chart.reflow()
-            }
-        } 
+        }
       },
-        inertia: true,
-        modifiers: [
-          interact.modifiers.restrictRect({
-            restriction: 'parent',
-            endOnly: true
-          })
-        ]
-      })
+      modifiers: [
+        // keep the edges inside the parent
+        interact.modifiers.restrictEdges({
+          outer: 'parent'
+        }),
+  
+        // minimum size
+        interact.modifiers.restrictSize({
+          min: { width: 20, height: 20 }
+        })
+      ],
+  
+      inertia: true
+    })
+    .draggable({
+      listeners: { move: function dragMoveListener (event) {
+        var target = event.target,
+            // keep the dragged position in the data-x/data-y attributes
+            x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+            y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+    
+        // translate the element
+        target.style.webkitTransform =
+        target.style.transform =
+          'translate(' + x + 'px, ' + y + 'px)';
+    
+        // update the posiion attributes
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+
+          // Resize the chart when the
+          // the container is resized
+          if(chart){
+            chart.reflow()
+          }
+      } 
+    },
+      inertia: true,
+      modifiers: [
+        interact.modifiers.restrictRect({
+          restriction: 'parent',
+          endOnly: true
+        })
+      ]
+    })
 }
 
 function makeDraggable(cssSelector){
