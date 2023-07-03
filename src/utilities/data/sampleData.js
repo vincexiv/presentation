bb6500c03b9a73c9e5a6b351da17d2a39c480408
Vincent <makokhavomondi@gmail.com>
2023-07-03 10:29:58 +0300

    Update sample data to include layout info

--- src/utilities/data/sampleData.js
+++ src/utilities/data/sampleData.js
@@ -4,7 +4,8 @@ import { monthlyAverageTemperature } from "./highchartCharts";
 // canvas. In this example, there is only one object in the canvas, but it could be more
 const sampleData = [
     {
-        id: 3,
+        id: 4,
+        layout: 'LAYOUT_16x9',
         objects: [
             { 
                 id: 1, 
@@ -15,26 +16,26 @@ const sampleData = [
                     placeholder: "Click to add title", 
                     style: {
                         fontSize: "2rem",
-                        verticalAlign: "middle"
+                        // verticalAlign: "middle"
                     }
                 }
             },
             { 
                 id: 2, 
+                typeDetails: { category: "content" },
                 style: {top: "30%", left: "5%", width: "44%", height: "65%" },
                 content: {
-                    type: "chart", 
-                    placeholder: "Click to add chart",
-                    value: monthlyAverageTemperature
+                    type: "text", 
+                    placeholder: "Click to add text", 
                 }
             },
             { 
                 id: 3, 
-                typeDetails: { category: "content" },
                 style: {top: "30%", left: "51%", width: "44%", height: "65%" },
                 content: {
-                    type: "text", 
-                    placeholder: "Click to add text", 
+                    type: "chart", 
+                    placeholder: "Click to add chart", 
+                    value: monthlyAverageTemperature
                 }
             }
         ]
